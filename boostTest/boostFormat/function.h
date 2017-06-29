e92c18235c409dc97d7e6aea33ab2b33de518463
hjw21century <hjw21century@163.com>
2017-06-30 00:33:26 +0800

    增加Format示例代码

---
+++ boostTest/boostFormat/function.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int sample_advanced();
+int sample_formats();
+int sample_new_features();
+int sample_userType();
---
+++ boostTest/boostFormat/main.cpp
@@ -0,0 +1,11 @@
+#include "function.h"
+#include <windows.h>
+
+int main()
+{
+	sample_advanced();
+	sample_formats();
+	sample_new_features();
+	sample_userType();
+	system("pause");
+}
