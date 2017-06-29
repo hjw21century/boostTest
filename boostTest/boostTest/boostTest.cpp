0dd27b118af1d362ef15e53bd2c0853d09c74322
hjw21century <hjw21century@163.com>
2017-06-29 23:25:38 +0800

    vs2015社区版boost测试程序项目

---
+++ boostTest/boostTest/boostTest.cpp
@@ -0,0 +1,16 @@
+// boostTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	calc_rules();
+	flight();
+	fun_local_date_time();
+	seconds_since_epoch();
+	simple_time_zone();
+    return 0;
+}
+
